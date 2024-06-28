d8046c05b0f6f60323564a0614e95c58b370a1c3
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2024-06-28 13:51:08 +0530

    Update variables.py

--- Day-03/examples/variables.py
+++ Day-03/examples/variables.py
@@ -1,1 +1,8 @@
+#without using variables
+print(ec2_instance_sample)
 
+#By using variables
+ec2_instance_name = ec2_instance_project
+print(ec2_instance_project)
+print(ec2_instance_project)
+print(ec2_instance_project)
