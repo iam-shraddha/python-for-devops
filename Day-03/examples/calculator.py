f24171b11d105ec4af70859a2e9e42bd2645b71f
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2024-06-28 14:14:35 +0530

    Update calculator.py

--- Day-03/examples/calculator.py
+++ Day-03/examples/calculator.py
@@ -1,1 +1,49 @@
+# Global scope variables
+a = 10
+b = 5
+
+def addition():
+  add = a+b
+  print("Addition of two numbers is " + str(add))
+def subtraction():
+  sub = a-b
+ print("Addition of two numbers is " + str(sub))
+addition()
+subtraction()
+#Output: 15 & 5
+
+###########################################
+
+# Local scope variables
+def addition():
+  a=10
+  b=5
+  add = a+b
+ print("Addition of two numbers is " + str(add))
+
+def subtraction():
+  sub = a-b
+ print("Addition of two numbers is " + str(sub))
+
+addition()
+subtraction()
+#Output: Error
+
+################################################
+
+# Global and Local scope variables
+a = 10
+b = 5
+def addition():
+  c = 15
+  add = a+b+c
+  print("Addition of two numbers is " + str(add))
+
+def subtraction():
+  sub = a-b
+  print("Addition of two numbers is " + str(sub))
+
+addition()
+subtraction()
+#Output: 30 & 5
 
