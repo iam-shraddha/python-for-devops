93519abc54dc76454eaadc418df451bcbf8a3303
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2024-06-28 21:18:36 +0530

    Update simple_calci.py

--- Day-04/examples/simple_calci.py
+++ Day-04/examples/simple_calci.py
@@ -1,1 +1,14 @@
+num1 = 20
+num2 = 10
+
+def addition():
+  add = num1 + num2
+  print("Addition of two numbers: " + str(add))
+
+def subtraction():
+  sub = num1 + num2
+  print("Subtraction of two numbers: " + str(sub))
+
+addition()
+subtraction()
 
