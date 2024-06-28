37af41ef2dd06ffe77d90460b891f65ed2359c90
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2024-06-28 20:25:43 +0530

    Update functions.py

--- Day-04/examples/functions.py
+++ Day-04/examples/functions.py
@@ -1,1 +1,25 @@
+#Program for calculator using function
 
+num1 = 20
+num2 = 10
+
+def addition():
+  add = num1 + num2
+  print("Addition of two numbers: " + str(add))
+
+def subtraction():
+  sub = num1 + num2
+  print("Subtraction of two numbers: " + str(sub))
+
+def multiplication():
+  mul = num1 * num2
+  print("Multition of two numbers: " + str(mul))
+
+def division():
+  div = num1 / num2
+  print("Division of two numbers: " + str(div))
+
+addition()
+subtraction()
+multiplication()
+division()
