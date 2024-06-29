bf7737cf8dd9545806d766ca51c44d94ca4b2a02
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2024-06-29 13:53:04 +0530

    Update while_loop.py

--- Day-09/examples/while_loop.py
+++ Day-09/examples/while_loop.py
@@ -1,4 +1,13 @@
+#While using num
+
 i = 0
 while(i<=10)
 print(i)
 
+#While using lists
+
+student_names = ["sonu","monu","tonu","shubu","pillu","chhotu","pajji"]
+i = 0
+while i < len(student_names):           #0 < 8
+   print(student_names[i])              #student_names[0]
+   i = i + 1                            #i = 0 + 1, i = 1
